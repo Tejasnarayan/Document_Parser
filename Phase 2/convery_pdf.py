cbb89003eb17196ee3c11e16ba80b9f8ce31f4b5
Tejas <narayantejas1998@gmail.com>
2022-02-28 21:52:03 +0530

    phase 2

---
+++ Phase 2/convery_pdf.py
@@ -0,0 +1,10 @@
+import PyPDF2
+	
+pdfFileObj = open('Document 4.2 .pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+	
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText())
+	
+pdfFileObj.close()
